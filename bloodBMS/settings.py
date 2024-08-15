fd4f78d69875e0c3473081c75be6fb5f522f3853
ndongchrist <christianhonore2003@gmail.com>
2024-08-15 20:47:11 +0100

    Removed postgres host

--- bloodBMS/settings.py
+++ bloodBMS/settings.py
@@ -90,7 +90,6 @@ DATABASES = {
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST'),
         'PORT': config('POSTGRES_PORT'),
     }
 }
