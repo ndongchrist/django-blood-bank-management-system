f72e38bed981f54fe4e677adf3441b4d462594f6
ndongchrist <christianhonore2003@gmail.com>
2024-08-15 21:06:26 +0100

    aDDED postgres host

--- bloodBMS/settings.py
+++ bloodBMS/settings.py
@@ -135,7 +135,7 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = str(BASE_DIR / "media")
