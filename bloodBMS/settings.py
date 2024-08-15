b2917cbf9e70b99e31250f99fbb243afb4b3608a
ndongchrist <christianhonore2003@gmail.com>
2024-08-15 20:25:42 +0100

    Added Staticfiles

--- bloodBMS/settings.py
+++ bloodBMS/settings.py
@@ -126,6 +126,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_ROOT = str(BASE_DIR / "media")
+MEDIA_URL = "/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
