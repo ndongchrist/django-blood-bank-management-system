50ce8c38b5816b95e9ae9822d4a46d2baf1a1cbf
ndongchrist <christianhonore2003@gmail.com>
2024-08-15 20:56:29 +0100

    aDDED postgres host

--- bloodBMS/settings.py
+++ bloodBMS/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
         'PORT': config('POSTGRES_PORT'),
     }
 }
