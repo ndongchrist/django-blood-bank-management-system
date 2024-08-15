fdc85b79dfcdbd44c876b79290d5bb7f74f7fe6e
ndongchrist <christianhonore2003@gmail.com>
2024-08-15 20:38:45 +0100

    Feat: Added postgres

--- bloodBMS/settings.py
+++ bloodBMS/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'bloodBMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT'),
     }
 }
+
 
 
 # Password validation
